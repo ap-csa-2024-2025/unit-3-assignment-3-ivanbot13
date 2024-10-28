e6472b2df5827474ee81d6c212805ab5a58c2283
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-28 11:20:13 -0700

    prob1

--- Main.java
+++ Main.java
@@ -4,15 +4,30 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Scanner sc = new Scanner(System.in);
     // TODO: Problem 1
-
+		System.out.println("Enter two numbers:");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+    if (y == 0)
+		{
+			System.out.println("Can't divide by 0");
+		}
+		else
+		{
+			double ratio = (double) x / y;
+			if (1 < ratio && ratio <= 8)
+			{
+				System.out.println("Ratio OK");
+			}
+		}
 
     // TODO: Problem 2
 
 
     // TODO: Problem 3
 
-    
+
   }
 
   // TODO: In-class assignment
